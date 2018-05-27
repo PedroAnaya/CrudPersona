0e6161edce92583cce78046f1edac9c557957bc4
PedroAnaya <anaya.sanchez.pedro@gmail.com>
2018-05-27 15:25:44 -0500

    Actualizacion Codigo

--- crud.py
+++ crud.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_mysqldb import MySQL
+from raven.contrib.flask import Sentry
 from datetime import datetime
- 
+
 app = Flask(__name__)
+
+
+sentry = Sentry(app, dsn='https://34b4dd07263949959a499ac3a98ea9d2:bba044ffa89544af98017e588e66e975@sentry.io/1210257')
+
+ADMINS = ['exceptionappweb@gmail.com']
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('127.0.0.1',
+                               'exceptionappweb@gmail.com',
+                               ADMINS, 'Error en la aplicaion')
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
